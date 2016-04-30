feabbf179fe504fd9613732c595164beccd8dcbb
Matt <MrZoraman@Gmail.com>
2016-04-30 01:08:24 -0600

    Polished and documented the YamlMergeException class.

--- src/main/java/com/lagopusempire/phiinae/YamlMergeException.java
+++ src/main/java/com/lagopusempire/phiinae/YamlMergeException.java
@@ -1,5 +1,13 @@
 package com.lagopusempire.phiinae;
 
+/**
+ * Thrown by the {@link com.lagopusempire.phiinae.IYamlConfig#merge(java.io.InputStream) IYamlConfig.merge()}
+ * method. See the documentation for that method for more details.
+ * @author Zora
+ */
 public class YamlMergeException extends RuntimeException {
-    
+    YamlMergeException() {
+        super("Yaml data type mismatch! See the javadoc of this library"
+                + "for more information.");
+    }
 }
